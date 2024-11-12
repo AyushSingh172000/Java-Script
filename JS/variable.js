298c09334df63a80b61470bee0f992540cd1e7f4
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2024-11-12 19:50:05 +0000

    c2

---
+++ JS/variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ayush17102000@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+
+//accountId = 2 // not allowed
+
+accountEmail = "aman123@gmail.com"
+accountPassword = "2125412"
+accountCity = "varanasi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
