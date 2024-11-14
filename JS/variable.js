ac0076de49e04e44fddb73bb5452d9a80d72bc6e
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2024-11-14 19:29:53 +0000

    variable

--- JS/variable.js
+++ JS/variable.js
@@ -11,4 +11,7 @@ accountCity = "varanasi"
 
 console.log(accountId);
 
+/*
+Prefer not to use var because of issue in block scope and functional scope*/
+
 console.table([accountId, accountEmail, accountPassword, accountCity])
