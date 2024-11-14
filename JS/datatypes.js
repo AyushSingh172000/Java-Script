8e9c0f9c3ac514c4fc3806f234b153836e5e4ea8
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2024-11-14 19:46:59 +0000

    datatype

---
+++ JS/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js codes as newer version.
+//alert(3+3) // we are using nodejs not browser
+
+// console.log(3+3)
+// console.log("Villain")
+
+let name = "Ayush"
+let age = 24
+let isLoggidIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standardalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof undefined); // output will be undefined
+console.log(typeof null); // output will be object
