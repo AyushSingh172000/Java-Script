3bf86c51006e6661b7979a3a128ca4a7021a08f1
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2025-01-03 00:39:44 +0530

    Add files via upload

---
+++ JS/Day-3 JS/autoglobal.js
@@ -0,0 +1,148 @@
+// "UseStrict"
+// var a=1;
+// let b;
+// console.log(a);
+// {
+//     b=20;
+//     console.log(a);
+//     console.log(b);
+    
+    
+// }
+// console.log(a);
+// console.log(b);
+
+// console.log("Start");
+// {
+//     a=10;
+//     console.log(a);
+    
+// }
+// console.log("Outside a Local Scope");
+// console.log(a);
+
+// for(var i=0;i<3;i++)
+// {
+//     setTimeout(()=>{
+//         console.log(i);
+        
+//     },1000)
+// }
+
+// var a=1;
+// console.log(a);
+// {
+//     b=20;
+//     console.log("Inside a Local Scope");
+//     console.log(a);
+//     console.log(b);
+// }
+// var b;
+// console.log(a);
+// console.log(b);
+
+// console.log("start");
+// var a=1;
+// let b=2;
+// const c=3;
+// {
+//     var d=40;
+//     b=20;
+//     const c=30;
+//     e=40;
+//     console.log("Inside the scope");
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+//     console.log(e);
+    
+    
+// }
+// console.log("Outside the scope");
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+//     console.log(e);
+//     console.log("End");
+
+// Q.1 Saket Bhatnagar website ?
+
+// console.log("start");
+// let a = 10;
+// var b = 20;
+// const c = 30;
+// {
+//   let a = 100;
+//   var b = 200;
+//   const c = 300;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log("end");
+
+// Q.2
+
+// console.log("start");
+// let a = 10;
+// console.log(b);
+// {
+//   var b = 200;
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+// Q.3
+
+// console.log("start");
+// let a = 10;
+// {
+//   console.log(a);
+//   let a = 10;
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+// Q.4
+
+// console.log("start");
+// var b = 20;
+// const c = 30;
+// {
+//   let a = 100;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+// Q.5
+
+// console.log("start");
+// let a = 10;
+// var b = 20;
+// const c = 30;
+// {
+//   let a = 10;
+//   console.log(a);
+//   const c = 300;
+//   console.log(b);
+//   b = 200;
+//   c = 30;
+//   console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+
+
---
+++ JS/Day-3 JS/script.js
@@ -0,0 +1,32 @@
+// console.log(0o07); // for octalDecimal
+// const n1="0o10";
+// const n2=7;
+// console.log(n1>n2);
+// console.log(n1<n2);
+// console.log(n1<=n2);
+// console.log(n1>=n2);
+
+// const n1="0x10"
+// const n2=16
+// console.log(n1==n2);
+
+// const user=prompt("Enter Your Name: ")
+// const res=(user && user!=="")?`Welcome ${user}`:`Invalid User Name`
+// console.log(res);
+
+// FIND THE BIGGEST B/W THREE NUMBERS?
+
+
+// const a=+prompt("Enter First Number: ");
+// const b=+prompt("Enter Second Number: ");
+// const c=+prompt("Enter Third Number: ");
+// const res=(a>b?a:b)
+// const result=(res>c?res:c)
+// console.log(`Biggest number among ${a}, ${b} and ${c} is):${result}`);
+
+// FIND THE LEAP YEAR?
+
+// const year=+prompt("Enter the Year");
+
+// const res=(year%100 !==0 && year%4 ===0)|| (year%400===0)?`Leap Year ${year}`:` ${year} Not a Leap Year`;
+// console.log(res);
