733d50585259b8b1c695cdb9deb9d5f614466650
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2025-01-03 00:40:25 +0530

    Add files via upload

---
+++ JS/Day-4 JS/Func.js
@@ -0,0 +1,69 @@
+// function fun(a,b){
+//     console.log(a);
+//     console.log(b);
+//     return
+    
+// }
+//  let x= fun(10,20)
+//  console.log(x);
+//  console.log(fun(30,40));
+ 
+//  var a=10;
+//  function task(m)
+//  {
+//     var b=20;
+//     let c=30;
+//     const d=40;
+//     console.log("Outside", a);
+//     console.log(m);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+//     return (m+b+c+d)
+    
+//  }
+//  let x=task(100)
+//  console.log(x);
+//  console.log("end");
+ 
+//  var user="Kallu";
+//  {
+//     console.log("Inside a local scope");
+//     function details(age,city)
+//     {
+//         var pin=110006;
+//         console.log(age);
+//         console.log(city);
+//         return `GM ${user}`;
+        
+//     }
+    
+//  }
+//  let x=details(21,"Noida")
+//  console.log(x);
+ 
+// CASE:2 : Function is declared inside another function.
+
+    // console.log("Start");
+    // var user="Kallu";
+    // function outer(age, city)
+    // {
+    //     console.log("Inside Outer Function");
+    //     var pin=221005;
+    //     console.log(`user Name:${user}`);
+    //     function inner(m,n)
+    //     {
+    //         console.log("Inside Inner Function");
+    //         console.log(m);
+    //         console.log(n);
+    //     }
+    //     inner()
+    // }
+    // outer()
+    
+// var fname="Kallu";
+// function fun1(user){
+//     console.log(`username:${user}`);
+    
+// }
+// fun1(fname)
