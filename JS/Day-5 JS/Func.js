9a909edacc67b9288798403a8a25875c9bdb46c4
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2025-01-04 00:34:58 +0530

    Add files via upload

---
+++ JS/Day-5 JS/Func.js
@@ -0,0 +1,54 @@
+// function details(user){
+//     if(user.trim()!=="" && user.length!==0)
+//     {
+//         console.log(`Username:${user.trim()}`);
+        
+//     }else{
+//         console.error("Invalid username");
+        
+//     }
+// }
+// let username=prompt("Enter fullname");
+// details(username)
+
+// function details(user)
+// {
+//     (user.trim()!=="" && user.length!==0)?console.log(`Username:${user.trim()}`):console.error("Invalid username");    
+// }
+
+// let username=prompt("Enter Fullname");
+// details(username)
+
+// Case-1 
+
+// console.log("Start");
+// greet();
+// {
+//     function greet()
+//     {
+//         console.log("Namastey Developer");
+        
+//     }
+// }
+
+// Case-2 
+
+
+console.log("Start");
+var a=10;
+fun();
+function fun()
+{
+    b=2;
+    console.log("Inside a Function");
+    console.log(a);
+    console.log(b);
+    
+    
+}
+
+console.log("Outside a Function");
+console.log(a);
+console.log(b);
+console.log("End");
+
