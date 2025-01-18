5a7d536e2fbb84ecf99e32fceeb0d1a010af6afc
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2025-01-18 20:51:34 +0530

    Add files via upload

---
+++ JS/Day-11 JS/Arrow.js
@@ -0,0 +1,52 @@
+// const task1=function(){
+//     let key="acb"
+//     return (function(){
+//         console.log(key);
+        
+//     })
+// }
+
+// const fun=()=>task1;
+// let x=fun()
+// console.log(x);
+
+// Explict Type Arrow function
+
+const power = (base, expo)=>{
+    let pow = base ** expo;
+    console.log(`${base}^${expo} = ${pow}`);
+    return pow;
+    
+}
+
+let x=power(10,3)
+console.log(x);
+
+// const sum=(a,b)=>a+b;
+
+// let x=sum(5,5);
+// console.log(x)
+
+// const total=(a,b)=>console.log(a+b);
+// let y=total(5,5);
+// console.log(y)
+
+
+
+// const task1=function(){
+//     let count=1;
+//     return (function(){
+//                 return count++;
+//             })  
+// }
+
+// const fun=()=>task1();
+
+// let x=fun()
+// console.log(x)
+// console.log(x())
+// console.log(x())
+// console.log(x())
+
+// console.log(document)
+console.log(process)
