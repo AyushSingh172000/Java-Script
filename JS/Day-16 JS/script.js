e83366b8815f20302fad86d7c96873eb2ab38b2d
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2025-01-31 00:42:13 +0530

    Add files via upload

---
+++ JS/Day-16 JS/script.js
@@ -0,0 +1,38 @@
+//console.log("Hello Objects Babua");
+
+// let obj = {
+//     userName: "Villain",
+//     emailId: "villain@gmail.com",
+//     age: 26,
+//     isWorking: true,
+//     wishHbd: function(){
+//         console.log(`Happy Birthday ${this.userName}`);
+        
+//     }
+// }
+
+// console.log(obj.userName);
+// console.log(obj.emailId);
+// console.log(obj.age);
+
+// let randomObj = {
+//     5: "Villain",
+//     3: "Ayush",
+//     4: "Misthi",
+//     2: "Kalash",
+//     1: "Kaluwa"
+
+// }
+
+// console.log(randomObj);
+
+// const key = prompt("Enter the Key");
+// const key1 = prompt("Enter the 1 Key:");
+// const obj ={
+//     [key]:"Villain",    
+//     [key1]:25    
+
+// }
+
+// console.log(obj);
+
